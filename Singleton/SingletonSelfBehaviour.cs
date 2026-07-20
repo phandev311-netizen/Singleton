536aeac7c46afa0afd1873c7ee625818e70d5a4f
Phan Dev <phandev311@gmail.com>
2026-07-20 16:13:47 +0700

    Update SingletonSelfBehaviour.cs

--- Singleton/SingletonSelfBehaviour.cs
+++ Singleton/SingletonSelfBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TT
+namespace NH
 {
     public class SingletonSelfBehaviour<T> where T : MonoBehaviour
     {
